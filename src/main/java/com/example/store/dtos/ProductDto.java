9d44c1e1bcb7c4008a41fd4085229c728cca4c09
HitenChawda <chawdahiten30@gmail.com>
2025-05-18 00:54:01 +0530

    Product dto, mapper and controller added

---
+++ src/main/java/com/example/store/dtos/ProductDto.java
@@ -0,0 +1,17 @@
+package com.example.store.dtos;
+
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+
+@Data
+public class ProductDto {
+    private Long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private Byte categoryId;
+}
