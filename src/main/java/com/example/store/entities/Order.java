6bb1690eb22350d6d2c890d38510dd325300060b
HitenChawda <chawdahiten30@gmail.com>
2025-05-19 22:15:37 +0530

    added route to get a single order

--- src/main/java/com/example/store/entities/Order.java
+++ src/main/java/com/example/store/entities/Order.java
@@ -48,4 +48,8 @@ public class Order {
         });
         return order;
     }
+
+    public boolean isPlacedBy(User user) {
+        return this.customer.equals(user);
+    }
 }
--- src/main/java/com/example/store/services/OrderService.java
+++ src/main/java/com/example/store/services/OrderService.java
@@ -1,9 +1,11 @@
 package com.example.store.services;
 
 import com.example.store.dtos.OrderDto;
+import com.example.store.exceptions.OrderNotFoundException;
 import com.example.store.mappers.OrderMapper;
 import com.example.store.repositories.OrderRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,7 +19,17 @@ public class OrderService {
 
     public List<OrderDto> getAllOrders() {
         var user = authService.getCurrentUser();
-        var orders = orderRepository.getAllByCustomer(user);
+        var orders = orderRepository.getOrdersByCustomer(user);
         return orders.stream().map(orderMapper::toDto).toList();
+    }
+
+    public OrderDto getOrder(Long orderId) {
+        var order = orderRepository.getOrderWithItems(orderId)
+                .orElseThrow(OrderNotFoundException::new);
+        var user = authService.getCurrentUser();
+        if (!order.isPlacedBy(user)) {
+            throw new AccessDeniedException("You do not have permission to access this order.");
+        }
+        return orderMapper.toDto(order);
     }
 }
